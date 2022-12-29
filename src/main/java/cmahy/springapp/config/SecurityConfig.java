0e8164217ce2140a5e4f2fd2698ed3e4b195ff31
Christophe Mahy <christ.mahy@gmail.com>
2022-12-29 23:35:59 +0100

    Use security with User domain

---
+++ src/main/java/cmahy/springapp/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package cmahy.springapp.config;
+
+import cmahy.springapp.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return username -> userRepository.findByUsername(username)
+            .map(UserSecurityDetails::new)
+            .orElseThrow(() -> new UsernameNotFoundException(
+                "User '" + username + "' not found"
+            ));
+    }
+}
