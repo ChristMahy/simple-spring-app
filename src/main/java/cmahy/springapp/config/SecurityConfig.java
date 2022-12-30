68c113af13cd7ae9546c17467745e7fbb0dc879d
Christophe Mahy <christ.mahy@gmail.com>
2022-12-30 23:24:23 +0100

    Use login page for login form security.
    Disable cors, iframe protection and csrf

--- src/main/java/cmahy/springapp/config/SecurityConfig.java
+++ src/main/java/cmahy/springapp/config/SecurityConfig.java
@@ -3,10 +3,12 @@ package cmahy.springapp.config;
 import cmahy.springapp.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -22,5 +24,25 @@ public class SecurityConfig {
             .orElseThrow(() -> new UsernameNotFoundException(
                 "User '" + username + "' not found"
             ));
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+            .authorizeHttpRequests()
+            .requestMatchers(
+                "/design", "/design/**",
+                "/orders", "/orders/**"
+            ).hasRole("USER")
+            .requestMatchers("/", "/**").permitAll()
+            .and()
+            .formLogin()
+            .loginPage("/login")
+            .and()
+            .headers()
+            .frameOptions().sameOrigin()
+            .and()
+            .csrf().disable()
+            .build();
     }
 }
