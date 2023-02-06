bf45fb3dda89ddde0793e760fa6d33508e875897
Christophe Mahy <christ.mahy@gmail.com>
2023-02-06 23:42:44 +0100

    Fix h2-console csrf

--- src/main/java/cmahy/springapp/config/security/NormalLoginHttpSecurityConfig.java
+++ src/main/java/cmahy/springapp/config/security/NormalLoginHttpSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 import static cmahy.springapp.config.security.HttpSecurityConfig.ORDER_CONFIG_NORMAL_LOGIN;
 import static org.slf4j.LoggerFactory.getLogger;
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @Component
 @Order(ORDER_CONFIG_NORMAL_LOGIN)
@@ -17,11 +18,13 @@ public class NormalLoginHttpSecurityConfig implements HttpSecurityConfig {
     public HttpSecurity configure(HttpSecurity httpSecurity) throws Exception {
         LOG.info("{}, setup normal login configuration", HttpSecurityConfig.class.getSimpleName());
         return httpSecurity
-            .formLogin()
-            .loginPage("/login")
+            .csrf().ignoringRequestMatchers(toH2Console())
             .and()
             .headers()
             .frameOptions().sameOrigin()
+            .and()
+            .formLogin()
+            .loginPage("/login")
             .and();
     }
 }
