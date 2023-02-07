cbf72ad6d61801cc510ce887f988089b24bbf93a
Christophe Mahy <christ.mahy@gmail.com>
2023-02-07 13:11:24 +0100

    Transform UserSecurityDetails as a record

--- src/main/java/cmahy/springapp/config/security/UserSecurityDetails.java
+++ src/main/java/cmahy/springapp/config/security/UserSecurityDetails.java
@@ -8,12 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-public class UserSecurityDetails implements UserDetails {
-    private final User user;
-
-    public UserSecurityDetails(User user) {
-        this.user = user;
-    }
+public record UserSecurityDetails(User user) implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
