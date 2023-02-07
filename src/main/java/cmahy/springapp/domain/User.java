c2a8158cc97dc8edf96e0a49a10ec3b984a11268
Christophe Mahy <christ.mahy@gmail.com>
2023-02-07 13:10:23 +0100

    Add Lombok ToString.Exclude to password

--- src/main/java/cmahy/springapp/domain/User.java
+++ src/main/java/cmahy/springapp/domain/User.java
@@ -14,6 +14,7 @@ public class User {
     private Long id;
 
     private String username;
+    @ToString.Exclude
     private String password;
     private String fullName;
     private String street;
