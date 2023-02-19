7937fe7f550b4ece89afb5fac99a82cb66014f00
Christophe Mahy <christ.mahy@gmail.com>
2023-02-19 04:11:12 +0100

    Enable capability to modify a total or a partial order.
     - Create patch and put order end point
     - Add a find by id to taco order repository.

--- src/main/java/cmahy/springapp/controller/rest/TacoController.java
+++ src/main/java/cmahy/springapp/controller/rest/TacoController.java
@@ -5,9 +5,8 @@ import cmahy.springapp.repository.TacoRepository;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.TEXT_XML_VALUE;
@@ -31,5 +30,11 @@ public class TacoController {
         );
 
         return tacoRepository.findAll(pageable).getContent();
+    }
+
+    @PostMapping(consumes = APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco createTaco(@RequestBody Taco taco) {
+        return tacoRepository.save(taco);
     }
 }
--- src/main/java/cmahy/springapp/repository/OrderRepository.java
+++ src/main/java/cmahy/springapp/repository/OrderRepository.java
@@ -5,9 +5,12 @@ import cmahy.springapp.domain.User;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository {
     TacoOrder save(TacoOrder tacoOrder);
 
     List<TacoOrder> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
+
+    Optional<TacoOrder> findById(Long orderId);
 }
