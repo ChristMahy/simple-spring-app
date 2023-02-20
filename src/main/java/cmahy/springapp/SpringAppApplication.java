2e5e2581bad0f72f9a5460b54e3452d86fc69d4f
Christophe Mahy <christ.mahy@gmail.com>
2023-02-20 21:38:54 +0100

    Disable all schedulers

--- src/main/java/cmahy/springapp/SpringAppApplication.java
+++ src/main/java/cmahy/springapp/SpringAppApplication.java
@@ -2,10 +2,8 @@ package cmahy.springapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class SpringAppApplication {
 
 	public static void main(String[] args) {
