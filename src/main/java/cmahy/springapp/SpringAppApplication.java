0480ee4385a5eeb562759fa91e3954bebf458b06
Christophe Mahy <christ.mahy@gmail.com>
2023-02-20 21:35:19 +0100

    Consuming a rest service with RestTemplate - Process a request.
     - Create RestTemplate as Bean
     - Enable scheduling
     - Add Scheduler to process the call

--- src/main/java/cmahy/springapp/SpringAppApplication.java
+++ src/main/java/cmahy/springapp/SpringAppApplication.java
@@ -2,8 +2,10 @@ package cmahy.springapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringAppApplication {
 
 	public static void main(String[] args) {
