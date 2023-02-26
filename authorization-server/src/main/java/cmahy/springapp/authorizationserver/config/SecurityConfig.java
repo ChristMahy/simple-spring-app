bf10ed22b2513a9ee6049425a682f8112578a463
Christophe Mahy <christ.mahy@gmail.com>
2023-02-26 15:58:35 +0100

    Authorization module:  - Add devtools dependency, to activate h2-console end point  - Add security configuration for h2-console path

--- authorization-server/src/main/java/cmahy/springapp/authorizationserver/config/SecurityConfig.java
+++ authorization-server/src/main/java/cmahy/springapp/authorizationserver/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package cmahy.springapp.authorizationserver.config;
 
 import cmahy.springapp.authorizationserver.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,6 +11,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
@@ -20,6 +24,11 @@ public class SecurityConfig {
             .authorizeHttpRequests(authorizationRequest ->
                 authorizationRequest.anyRequest().authenticated()
             )
+            .csrf().ignoringRequestMatchers(toH2Console())
+            .and()
+            .headers()
+            .frameOptions().sameOrigin()
+            .and()
             .formLogin()
             .and().build();
     }
