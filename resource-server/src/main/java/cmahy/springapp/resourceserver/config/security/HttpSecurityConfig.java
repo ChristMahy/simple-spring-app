ae25e320e256fa0ec8a77eb23dcfb7352eb6fa5d
Christophe Mahy <christ.mahy@gmail.com>
2023-03-14 21:32:35 +0100

    Resource module
     - Change interface to final class, avoid extends and implementations

--- resource-server/src/main/java/cmahy/springapp/resourceserver/config/security/HttpSecurityConfig.java
+++ resource-server/src/main/java/cmahy/springapp/resourceserver/config/security/HttpSecurityConfig.java
@@ -2,10 +2,10 @@ package cmahy.springapp.resourceserver.config.security;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
-public interface HttpSecurityConfig {
-    int ORDER_CONFIG_NORMAL_LOGIN = 1;
-    int ORDER_CONFIG_OAUTH2_LOGIN = 2;
-    int ORDER_CONFIG_LOGOUT = 3;
+public final class HttpSecurityConfig {
+    public static final int ORDER_CONFIG_NORMAL_LOGIN = 1;
+    public static final int ORDER_CONFIG_OAUTH2_LOGIN = 2;
+    public static final int ORDER_CONFIG_LOGOUT = 3;
 
-    HttpSecurity configure(HttpSecurity httpSecurity) throws Exception;
+    private HttpSecurityConfig() {}
 }
