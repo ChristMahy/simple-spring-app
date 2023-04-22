677b7296a5348b81db01d8f450d753b63e57d85d
Christophe Mahy <christ.mahy@gmail.com>
2023-04-22 10:26:49 +0200

    RabbitMQ resource: Configure a serializer, use jackson lib

--- broker/rabbitmq/rabbitmq-resource/src/main/java/cmahy/springapp/rabbitmq/resource/config/RabbitMQConfigurer.java
+++ broker/rabbitmq/rabbitmq-resource/src/main/java/cmahy/springapp/rabbitmq/resource/config/RabbitMQConfigurer.java
@@ -1,7 +1,16 @@
 package cmahy.springapp.rabbitmq.resource.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfigurer {
+
+    @Bean
+    public MessageConverter messageConverter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
 }
