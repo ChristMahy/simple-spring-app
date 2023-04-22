b112ce1f35f57c431590f51e1a9e7e40c9b4d98b
Christophe Mahy <christ.mahy@gmail.com>
2023-04-22 10:24:17 +0200

    RabbitMQ resource: Setup config, implement service which send message through a queue

--- broker/rabbitmq/rabbitmq-resource/src/main/java/cmahy/springapp/rabbitmq/resource/service/RabbitMQService.java
+++ broker/rabbitmq/rabbitmq-resource/src/main/java/cmahy/springapp/rabbitmq/resource/service/RabbitMQService.java
@@ -1,12 +1,23 @@
 package cmahy.springapp.rabbitmq.resource.service;
 
+import cmahy.springapp.rabbitmq.resource.RabbitMQQueue;
 import cmahy.springapp.rabbitmq.resource.domain.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RabbitMQService {
 
+    private final RabbitTemplate rabbit;
+
+    public RabbitMQService(RabbitTemplate rabbit) {
+        this.rabbit = rabbit;
+    }
+
     public void sendMessage(Message message) {
-        throw new IllegalStateException("Not yet implemented");
+        rabbit.convertAndSend(
+            RabbitMQQueue.TACOCLOUD_MESSAGE,
+            message
+        );
     }
 }
