e5c73e411a50fcdb28dbb07ba1382063d56f3ed6
Christophe Mahy <christ.mahy@gmail.com>
2023-06-24 22:51:08 +0200

    Integration - Replace definition with single bean and DSL helpers

--- web-server/resource-server/src/main/java/cmahy/springapp/resourceserver/config/integration/FileWriterIntegrationConfigurer.java
+++ web-server/resource-server/src/main/java/cmahy/springapp/resourceserver/config/integration/FileWriterIntegrationConfigurer.java
@@ -4,49 +4,34 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.integration.annotation.Transformer;
-import org.springframework.integration.channel.DirectChannel;
-import org.springframework.integration.core.GenericTransformer;
-import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.MessageChannels;
+import org.springframework.integration.file.dsl.Files;
 import org.springframework.integration.file.support.FileExistsMode;
-import org.springframework.messaging.MessageChannel;
 
-import java.io.File;
 import java.io.IOException;
 
 @Configuration
 public class FileWriterIntegrationConfigurer {
 
     @Bean
-    @Transformer(inputChannel = "textInChannel", outputChannel = "fileWriterChannel")
-    public GenericTransformer<String, String> upperCaseTransformer() {
-        return String::toUpperCase;
-    }
-
-    @Bean
-    @ServiceActivator(inputChannel = "fileWriterChannel")
-    public FileWritingMessageHandler fileWriter(@Value("spring.application.name") String appName) throws IOException {
-        File file = java.nio.file.Files.createTempDirectory(StringUtils.isBlank(appName) ? "spring-app-test" : appName).toFile();
-
-        FileWritingMessageHandler handler = new FileWritingMessageHandler(file);
-
-        handler.setExpectReply(false);
-        handler.setFileExistsMode(FileExistsMode.APPEND);
-        handler.setAppendNewLine(true);
-
-        return handler;
-    }
-
-    // Non obligatoire, car construit par défaut
-    @Bean
-    public MessageChannel textInChannel() {
-        return new DirectChannel();
-    }
-
-    // Non obligatoire, car construit par défaut
-    @Bean
-    public MessageChannel fileWriterChannel() {
-        return new DirectChannel();
+    public IntegrationFlow fileWriterFlow(@Value("spring.application.name") String appName) {
+        return (flow) -> {
+            try {
+                flow
+                    .channel(MessageChannels.direct("textInChannel"))
+                    .<String, String>transform(String::toUpperCase)
+                    .handle(
+                        Files
+                            .outboundAdapter(
+                                java.nio.file.Files.createTempDirectory(StringUtils.isBlank(appName) ? "spring-app-test" : appName).toFile()
+                            )
+                            .fileExistsMode(FileExistsMode.APPEND)
+                            .appendNewLine(true)
+                    );
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
     }
 }
