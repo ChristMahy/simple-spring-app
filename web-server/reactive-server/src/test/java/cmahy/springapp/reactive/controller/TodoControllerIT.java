37d47e66b479b2e05890206a4d328aed83ae4b99
Christophe Mahy <christ.mahy@gmail.com>
2023-07-08 13:44:23 +0200

    Add creation endpoint integration test

--- web-server/reactive-server/src/test/java/cmahy/springapp/reactive/controller/TodoControllerIT.java
+++ web-server/reactive-server/src/test/java/cmahy/springapp/reactive/controller/TodoControllerIT.java
@@ -1,17 +1,17 @@
 package cmahy.springapp.reactive.controller;
 
 import cmahy.springapp.reactive.domain.Todo;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.test.web.servlet.MockMvc;
+import reactor.core.publisher.Mono;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
 @AutoConfigureWebTestClient
@@ -28,6 +28,32 @@ class TodoControllerIT {
                 .expectStatus().isOk()
                 .expectBodyList(Todo.class)
                 .hasSize(100);
+        });
+    }
+
+    @Test
+    void create() {
+        assertDoesNotThrow(() -> {
+            final Todo todo = new Todo();
+
+            todo.setTitle("New title...");
+            todo.setDescription("New description...");
+
+            Mono<Todo> unsavedTodo = Mono.just(todo);
+
+            EntityExchangeResult<Todo> actualResult = testClient.post().uri("/todo")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(unsavedTodo, Todo.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Todo.class)
+                .returnResult();
+
+            assertThat(actualResult.getResponseBody()).isNotNull();
+
+            assertThat(actualResult.getResponseBody().getId()).isNotNull();
+            assertThat(actualResult.getResponseBody().getDescription()).isEqualTo(todo.getDescription());
+            assertThat(actualResult.getResponseBody().getTitle()).isEqualTo(todo.getTitle());
         });
     }
 }
