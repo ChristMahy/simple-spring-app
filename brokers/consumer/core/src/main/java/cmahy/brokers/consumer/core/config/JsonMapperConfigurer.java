56c8da4980d434ac77ef5daa5eb291418cc3877e
Christophe Mahy <christ.mahy@gmail.com>
2023-09-21 22:39:32 +0200

    Consumer: Add common lib, create object mapper as bean

---
+++ brokers/consumer/core/src/main/java/cmahy/brokers/consumer/core/config/JsonMapperConfigurer.java
@@ -0,0 +1,17 @@
+package cmahy.brokers.consumer.core.config;
+
+import cmahy.common.json.JsonMapperFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class JsonMapperConfigurer {
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        return JsonMapperFactory.create();
+    }
+}
