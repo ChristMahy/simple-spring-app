68c9856f0ca637fabf17db21104894c732717972
Christophe Mahy <christ.mahy@gmail.com>
2023-09-28 23:13:40 +0200

    Consumer: Switch RestController place

--- brokers/consumer/core/src/main/java/cmahy/brokers/consumer/core/adapter/api/message/MessageApiImpl.java
+++ brokers/consumer/core/src/main/java/cmahy/brokers/consumer/core/adapter/api/message/MessageApiImpl.java
@@ -1,15 +1,15 @@
 package cmahy.brokers.consumer.core.adapter.api.message;
 
 import cmahy.brokers.consumer.api.message.MessageApi;
+import cmahy.brokers.consumer.api.vo.output.MessageOutputApiVo;
 import cmahy.brokers.consumer.core.adapter.mapper.message.MessageAdapterMapper;
 import cmahy.brokers.consumer.core.application.query.message.GetAllMessageQuery;
-import cmahy.brokers.consumer.api.vo.output.MessageOutputApiVo;
-import jakarta.inject.Named;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@Named
+@RestController
 public class MessageApiImpl implements MessageApi {
 
     private final GetAllMessageQuery getAllMessageQuery;
