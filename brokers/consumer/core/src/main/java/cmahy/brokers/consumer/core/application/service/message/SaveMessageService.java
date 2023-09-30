89a15de1cb7faf5bcf332d0fb8865eeab8121e82
Christophe Mahy <christ.mahy@gmail.com>
2023-09-30 14:17:57 +0200

    Consumer: SaveMessageService doesn't need to delete message, it's not her job

--- brokers/consumer/core/src/main/java/cmahy/brokers/consumer/core/application/service/message/SaveMessageService.java
+++ brokers/consumer/core/src/main/java/cmahy/brokers/consumer/core/application/service/message/SaveMessageService.java
@@ -11,18 +11,15 @@ import java.util.Optional;
 public class SaveMessageService {
     
     private final FindByIdMessageService findById;
-    private final DeleteMessageService delete;
     private final CreateMessageService create;
     private final UpdateMessageService update;
 
     public SaveMessageService(
         FindByIdMessageService findById,
-        DeleteMessageService delete,
         CreateMessageService create,
         UpdateMessageService update
     ) {
         this.findById = findById;
-        this.delete = delete;
         this.create = create;
         this.update = update;
     }
@@ -31,8 +28,6 @@ public class SaveMessageService {
         Optional<Message> message = findById.execute(id);
 
         if (message.isPresent()) {
-            delete.execute(message.get());
-
             return update.execute(input);
         } else {
             return create.execute(input);
