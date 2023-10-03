5e04e70b244f7c71a7dab508e1921a41658818b6
Christophe Mahy <christ.mahy@gmail.com>
2023-10-03 08:40:16 +0200

    Publisher: Define message api

--- brokers/publisher/api/src/main/java/cmahy/brokers/publisher/api/message/MessageApi.java
+++ brokers/publisher/api/src/main/java/cmahy/brokers/publisher/api/message/MessageApi.java
@@ -1,13 +1,25 @@
 package cmahy.brokers.publisher.api.message;
 
 import cmahy.brokers.publisher.api.UriConstant;
+import cmahy.brokers.publisher.api.vo.id.MessageApiId;
+import cmahy.brokers.publisher.api.vo.input.MessageInputApiVo;
 import cmahy.brokers.publisher.api.vo.output.MessageOutputApiVo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping(path = UriConstant.BASE + UriConstant.Message.BASE)
 public interface MessageApi {
 
     @GetMapping
     Iterable<MessageOutputApiVo> allMessages();
+
+    @PostMapping
+    MessageOutputApiVo create(@RequestBody MessageInputApiVo input);
+
+    @PutMapping(path = "/{id}")
+    MessageOutputApiVo update(@RequestBody MessageInputApiVo input, @PathVariable MessageApiId id);
+
+    @DeleteMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void delete(@PathVariable MessageApiId id);
 }
