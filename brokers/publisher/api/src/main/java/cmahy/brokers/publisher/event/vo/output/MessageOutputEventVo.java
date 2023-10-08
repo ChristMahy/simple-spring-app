58798d99833c4bc537ea1611676db1533657b919
Christophe Mahy <christ.mahy@gmail.com>
2023-10-08 16:52:57 +0200

    Publisher-jms: Add id to output message class

--- brokers/publisher/api/src/main/java/cmahy/brokers/publisher/event/vo/output/MessageOutputEventVo.java
+++ brokers/publisher/api/src/main/java/cmahy/brokers/publisher/event/vo/output/MessageOutputEventVo.java
@@ -1,8 +1,11 @@
 package cmahy.brokers.publisher.event.vo.output;
 
+import cmahy.brokers.publisher.event.vo.id.MessageEventId;
+
 import java.time.LocalDateTime;
 
 public record MessageOutputEventVo(
+    MessageEventId id,
     String message,
     LocalDateTime createdAt
 ) { }
