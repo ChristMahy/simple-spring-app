ebcedc4c90bb50c1bc834260ed552c4f63232bf1
Christophe Mahy <christ.mahy@gmail.com>
2023-10-08 16:48:20 +0200

    Consumer-api: Remove JsonUnwrapped, see Readme.md at project root directory for more information

--- brokers/consumer/api/src/main/java/cmahy/brokers/consumer/event/vo/input/MessageInputEventVo.java
+++ brokers/consumer/api/src/main/java/cmahy/brokers/consumer/event/vo/input/MessageInputEventVo.java
@@ -1,14 +1,11 @@
 package cmahy.brokers.consumer.event.vo.input;
 
 import cmahy.brokers.consumer.event.vo.id.MessageEventId;
-import com.fasterxml.jackson.annotation.JsonUnwrapped;
 
 import java.time.LocalDateTime;
 
 public record MessageInputEventVo(
-    @JsonUnwrapped
     MessageEventId id,
     String message,
     LocalDateTime createdAt
-) {
-}
+) { }
