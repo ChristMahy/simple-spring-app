758a3417e199715b3d02f08f484ee9b0826d49fa
Christophe Mahy <christ.mahy@gmail.com>
2023-10-08 16:51:25 +0200

    Consumer-jms: Add some logs

--- brokers/consumer/jms/src/main/java/cmahy/brokers/consumer/jms/listener/MessageModificationJmsListener.java
+++ brokers/consumer/jms/src/main/java/cmahy/brokers/consumer/jms/listener/MessageModificationJmsListener.java
@@ -1,16 +1,17 @@
 package cmahy.brokers.consumer.jms.listener;
 
-import cmahy.brokers.consumer.api.vo.input.MessageInputApiVo;
 import cmahy.brokers.consumer.event.message.ModificationMessageListener;
 import cmahy.brokers.consumer.event.vo.input.MessageInputEventVo;
 import cmahy.brokers.consumer.jms.config.JmsQueue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class MessageModificationJmsListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MessageModificationJmsListener.class);
 
     private final ModificationMessageListener listener;
 
@@ -20,6 +21,8 @@ public class MessageModificationJmsListener {
 
     @JmsListener(destination = JmsQueue.MESSAGE_QUEUE_NAME + ".modify")
     public void receiveMessage(MessageInputEventVo message) {
+        LOG.debug("Event received, message modification <{}>", message);
+
         listener.execute(message);
     }
 }
