099c7e6afccac87d715e1bbf7f1426841b85cd48
Christophe Mahy <christ.mahy@gmail.com>
2023-10-14 10:42:40 +0200

    Consumer-kafka - Implement delete and modify listeners

---
+++ brokers/consumer/kafka/src/main/java/cmahy/brokers/consumer/kafka/listener/MessageDeleteKafkaListener.java
@@ -0,0 +1,50 @@
+package cmahy.brokers.consumer.kafka.listener;
+
+
+import cmahy.brokers.consumer.event.message.DeletionMessageListener;
+import cmahy.brokers.consumer.event.vo.id.MessageEventId;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class MessageDeleteKafkaListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MessageDeleteKafkaListener.class);
+
+    private final DeletionMessageListener listener;
+    private final ObjectMapper jsonMapper;
+
+    public MessageDeleteKafkaListener(
+        DeletionMessageListener listener,
+        ObjectMapper jsonMapper
+    ) {
+        this.listener = listener;
+        this.jsonMapper = jsonMapper;
+    }
+
+    @KafkaListener(topics = "message.queue.delete")
+    public void receiveMessage(
+        byte[] message,
+        ConsumerRecord<String, byte[]> record
+    ) {
+        LOG.info(
+            "Received from partition {} with timestamp {}",
+            record.partition(),
+            record.timestamp()
+        );
+
+        try {
+            listener.execute(
+                jsonMapper.readValue(message, MessageEventId.class)
+            );
+        } catch (IOException e) {
+            LOG.error(e.getMessage(), e);
+        }
+    }
+}
---
+++ brokers/consumer/kafka/src/main/java/cmahy/brokers/consumer/kafka/listener/MessageModificationKafkaListener.java
@@ -0,0 +1,47 @@
+package cmahy.brokers.consumer.kafka.listener;
+
+import cmahy.brokers.consumer.event.message.ModificationMessageListener;
+import cmahy.brokers.consumer.event.vo.input.MessageInputEventVo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class MessageModificationKafkaListener {
+
+    private final ModificationMessageListener listener;
+    private final ObjectMapper jsonMapper;
+
+    public MessageModificationKafkaListener(
+        ModificationMessageListener listener,
+        ObjectMapper jsonMapper
+    ) {
+        this.listener = listener;
+        this.jsonMapper = jsonMapper;
+    }
+
+    @KafkaListener(topics = "message.queue.modify")
+    public void receiveMessage(
+        byte[] message,
+        ConsumerRecord<String, byte[]> record
+    ) {
+        /*
+        TODO: LOG.info(
+            "Received from partition {} with timestamp {}",
+            record.partition(),
+            record.timestamp()
+        );
+        */
+
+        try {
+            listener.execute(
+                jsonMapper.readValue(message, MessageInputEventVo.class)
+            );
+        } catch (IOException e) {
+            // TODO: LOG.error(e.getMessage(), e);
+        }
+    }
+}
