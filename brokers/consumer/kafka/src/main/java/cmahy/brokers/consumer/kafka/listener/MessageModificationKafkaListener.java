09e0147f5d43551993d3ee8829de8d2e521fe958
Christophe Mahy <christ.mahy@gmail.com>
2023-10-14 10:58:46 +0200

    Publisher-kafka - Modification event, enable some logs

--- brokers/consumer/kafka/src/main/java/cmahy/brokers/consumer/kafka/listener/MessageModificationKafkaListener.java
+++ brokers/consumer/kafka/src/main/java/cmahy/brokers/consumer/kafka/listener/MessageModificationKafkaListener.java
@@ -4,6 +4,8 @@ import cmahy.brokers.consumer.event.message.ModificationMessageListener;
 import cmahy.brokers.consumer.event.vo.input.MessageInputEventVo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,8 @@ import java.io.IOException;
 
 @Service
 public class MessageModificationKafkaListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MessageModificationKafkaListener.class);
 
     private final ModificationMessageListener listener;
     private final ObjectMapper jsonMapper;
@@ -28,20 +32,18 @@ public class MessageModificationKafkaListener {
         byte[] message,
         ConsumerRecord<String, byte[]> record
     ) {
-        /*
-        TODO: LOG.info(
+        LOG.info(
             "Received from partition {} with timestamp {}",
             record.partition(),
             record.timestamp()
         );
-        */
 
         try {
             listener.execute(
                 jsonMapper.readValue(message, MessageInputEventVo.class)
             );
         } catch (IOException e) {
-            // TODO: LOG.error(e.getMessage(), e);
+            LOG.error(e.getMessage(), e);
         }
     }
 }
