538c55290dad64950aa9bf53a4d0bef9d08e2a21
Christophe Mahy <christ.mahy@gmail.com>
2023-10-14 18:09:13 +0200

    Consumer-api - Message output ignore null properties

--- brokers/consumer/api/src/main/java/cmahy/brokers/consumer/api/vo/output/MessageOutputApiVo.java
+++ brokers/consumer/api/src/main/java/cmahy/brokers/consumer/api/vo/output/MessageOutputApiVo.java
@@ -1,10 +1,12 @@
 package cmahy.brokers.consumer.api.vo.output;
 
 import cmahy.brokers.consumer.api.vo.id.MessageApiId;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 
 import java.time.LocalDateTime;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record MessageOutputApiVo(
     @JsonUnwrapped
     MessageApiId id,
