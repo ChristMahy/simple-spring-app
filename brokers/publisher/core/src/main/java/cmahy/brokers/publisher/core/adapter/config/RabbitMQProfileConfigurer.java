f5da4da0b3de98fdaadb861fdf6fe65fdcf7489e
Christophe Mahy <christ.mahy@gmail.com>
2023-10-20 23:28:57 +0200

    Publisher-core - Disable Rabbit MQ by default, enable it when needed

--- brokers/publisher/core/src/main/java/cmahy/brokers/publisher/core/adapter/config/RabbitMQProfileConfigurer.java
+++ brokers/publisher/core/src/main/java/cmahy/brokers/publisher/core/adapter/config/RabbitMQProfileConfigurer.java
@@ -1,9 +1,11 @@
 package cmahy.brokers.publisher.core.adapter.config;
 
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.context.annotation.*;
 
 @Profile("rabbitmq")
 @Configuration
+@Import({RabbitAutoConfiguration.class})
 @ComponentScan("cmahy.brokers.publisher.rabbitmq")
 public class RabbitMQProfileConfigurer {
 }
