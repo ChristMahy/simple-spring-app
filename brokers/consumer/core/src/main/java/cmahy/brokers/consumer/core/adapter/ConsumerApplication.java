963e175e9508dc3ef205866457a062563922a9c5
Christophe Mahy <christ.mahy@gmail.com>
2023-10-20 23:28:48 +0200

    Consumer-core - Disable Rabbit MQ by default, enable it when needed

--- brokers/consumer/core/src/main/java/cmahy/brokers/consumer/core/adapter/ConsumerApplication.java
+++ brokers/consumer/core/src/main/java/cmahy/brokers/consumer/core/adapter/ConsumerApplication.java
@@ -2,9 +2,10 @@ package cmahy.brokers.consumer.core.adapter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = { "cmahy.brokers.consumer.core" }, exclude = { ArtemisAutoConfiguration.class })
+@SpringBootApplication(scanBasePackages = { "cmahy.brokers.consumer.core" }, exclude = { ArtemisAutoConfiguration.class, RabbitAutoConfiguration.class })
 public class ConsumerApplication {
 
     public static void main(String[] args) {
--- brokers/consumer/core/src/main/java/cmahy/brokers/consumer/core/adapter/config/RabbitMQPushProfileConfigurer.java
+++ brokers/consumer/core/src/main/java/cmahy/brokers/consumer/core/adapter/config/RabbitMQPushProfileConfigurer.java
@@ -1,11 +1,13 @@
 package cmahy.brokers.consumer.core.adapter.config;
 
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.*;
 
 @Profile("rabbitmq")
 @ConditionalOnProperty(name = "application.rabbitmq.consumer.type", havingValue = "push")
 @Configuration
+@Import({RabbitAutoConfiguration.class})
 @ComponentScan("cmahy.brokers.consumer.rabbitmq.push")
 public class RabbitMQPushProfileConfigurer {
 }
