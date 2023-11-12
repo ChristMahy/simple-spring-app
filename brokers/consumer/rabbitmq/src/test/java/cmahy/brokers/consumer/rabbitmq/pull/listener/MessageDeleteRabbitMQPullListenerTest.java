6c54be15b19413d549672fce8b9abd69c595e624
Christophe Mahy <christ.mahy@gmail.com>
2023-11-12 22:45:56 +0100

    Common-rabbitmq - Implement all unit tests, follow by refactoring

---
+++ brokers/consumer/rabbitmq/src/test/java/cmahy/brokers/consumer/rabbitmq/pull/listener/MessageDeleteRabbitMQPullListenerTest.java
@@ -0,0 +1,13 @@
+package cmahy.brokers.consumer.rabbitmq.pull.listener;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageDeleteRabbitMQPullListenerTest {
+
+    @Test
+    void execute() {
+        throw new IllegalStateException("Not yet implemented !");
+    }
+}
---
+++ brokers/consumer/rabbitmq/src/test/java/cmahy/brokers/consumer/rabbitmq/push/listener/MessageModificationRabbitMQPushListenerTest.java
@@ -0,0 +1,13 @@
+package cmahy.brokers.consumer.rabbitmq.push.listener;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageModificationRabbitMQPushListenerTest {
+
+    @Test
+    void execute() {
+        throw new IllegalStateException("Not yet implemented !");
+    }
+}
