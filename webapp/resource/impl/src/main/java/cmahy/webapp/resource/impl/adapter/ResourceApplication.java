d62271e55faeb75e44a869bafcd4e7e3eb7922a9
Christophe Mahy <christ.mahy@gmail.com>
2024-02-13 08:58:25 +0100

    Resource-Impl - Define domain taco's entities, define database (h2), add scan entities

--- webapp/resource/impl/src/main/java/cmahy/webapp/resource/impl/adapter/ResourceApplication.java
+++ webapp/resource/impl/src/main/java/cmahy/webapp/resource/impl/adapter/ResourceApplication.java
@@ -2,10 +2,10 @@ package cmahy.webapp.resource.impl.adapter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication(
-    scanBasePackages = { "cmahy.webapp.resource.impl" }
-)
+@SpringBootApplication(scanBasePackages = { "cmahy.webapp.resource.impl" })
+@EntityScan("cmahy.webapp.resource.impl.*")
 public class ResourceApplication {
 
     public static void main(String[] args) {
---
+++ webapp/resource/impl/src/main/java/cmahy/webapp/resource/impl/domain/taco/Taco.java
@@ -0,0 +1,71 @@
+package cmahy.webapp.resource.impl.domain.taco;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.util.*;
+
+@Entity
+public class Taco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Date createdAt = new Date();
+
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
+    private String name;
+
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
+    @ManyToMany
+    private List<Ingredient> ingredients = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public void addIngredient(Ingredient ingredient) {
+        ingredients.add(ingredient);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.JSON_STYLE)
+            .append("name", name)
+            .append("ingredients", ingredients)
+            .build();
+    }
+}
