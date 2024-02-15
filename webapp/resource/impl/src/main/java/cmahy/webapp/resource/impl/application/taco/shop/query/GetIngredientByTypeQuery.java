3be1b7a584192547e88d78e93b39a8b713354e54
Christophe Mahy <christ.mahy@gmail.com>
2024-02-15 22:27:36 +0100

    Resource - Get ingredients by type, create query and repository method

---
+++ webapp/resource/impl/src/main/java/cmahy/webapp/resource/impl/application/taco/shop/query/GetIngredientByTypeQuery.java
@@ -0,0 +1,35 @@
+package cmahy.webapp.resource.impl.application.taco.shop.query;
+
+import cmahy.common.annotation.Query;
+import cmahy.webapp.resource.impl.application.taco.shop.mapper.IngredientOutputMapper;
+import cmahy.webapp.resource.impl.application.taco.shop.repository.IngredientRepository;
+import cmahy.webapp.resource.impl.application.taco.shop.vo.output.IngredientOutputAppVo;
+import cmahy.webapp.resource.impl.domain.taco.Ingredient;
+import jakarta.inject.Named;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Query
+@Named
+public class GetIngredientByTypeQuery {
+
+    private final IngredientRepository ingredientRepository;
+    private final IngredientOutputMapper ingredientOutputMapper;
+
+    public GetIngredientByTypeQuery(
+        IngredientRepository ingredientRepository,
+        IngredientOutputMapper ingredientOutputMapper
+    ) {
+        this.ingredientRepository = ingredientRepository;
+        this.ingredientOutputMapper = ingredientOutputMapper;
+    }
+
+    public Set<IngredientOutputAppVo> execute(Ingredient.Type type) {
+        return ingredientRepository
+            .findByType(type)
+            .stream()
+            .map(ingredientOutputMapper::map)
+            .collect(Collectors.toSet());
+    }
+}
--- webapp/resource/impl/src/main/java/cmahy/webapp/resource/impl/application/taco/shop/repository/IngredientRepository.java
+++ webapp/resource/impl/src/main/java/cmahy/webapp/resource/impl/application/taco/shop/repository/IngredientRepository.java
@@ -1,4 +1,10 @@
 package cmahy.webapp.resource.impl.application.taco.shop.repository;
 
+import cmahy.webapp.resource.impl.domain.taco.Ingredient;
+
+import java.util.Set;
+
 public interface IngredientRepository {
+
+    Set<Ingredient> findByType(Ingredient.Type type);
 }
