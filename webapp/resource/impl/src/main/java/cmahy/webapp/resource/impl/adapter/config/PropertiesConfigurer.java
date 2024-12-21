6878005e60857e3707c9d704ef6030b35450894c
Christophe Mahy <christ.mahy@gmail.com>
2024-12-22 00:10:19 +0100

    Resource - Impl - Convertor, replace concrete class with bean definition

--- webapp/resource/impl/src/main/java/cmahy/webapp/resource/impl/adapter/config/PropertiesConfigurer.java
+++ webapp/resource/impl/src/main/java/cmahy/webapp/resource/impl/adapter/config/PropertiesConfigurer.java
@@ -1,5 +1,7 @@
 package cmahy.webapp.resource.impl.adapter.config;
 
+import cmahy.common.converter.StringToByteConverter;
+import cmahy.common.converter.factory.StringToByteConverterFactory;
 import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,15 +14,17 @@ import java.util.Optional;
 public class PropertiesConfigurer {
 
     @Bean
+    public StringToByteConverter stringToBytesConverterImpl() {
+        return StringToByteConverterFactory.create();
+    }
+
+    @Bean
     @ConfigurationPropertiesBinding
-    public Converter<String, byte[]> stringToBytesConverter() {
-        return new Converter<String, byte[]>() {
+    public Converter<String, byte[]> stringToBytesConverter(StringToByteConverter converter) {
+        return new Converter<>() {
             @Override
             public byte[] convert(String source) {
-                return Optional
-                    .ofNullable(source)
-                    .map(s -> s.getBytes(StandardCharsets.UTF_8))
-                    .orElse(null);
+                return converter.convert(source);
             }
         };
     }
