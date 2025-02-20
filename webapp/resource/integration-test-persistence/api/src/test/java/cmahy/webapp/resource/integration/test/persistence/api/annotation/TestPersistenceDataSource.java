2194208dbc1b9e73ae276471259bf9bcb9c51f40
Christophe Mahy <christ.mahy@gmail.com>
2025-02-20 18:30:49 +0100

    Remove integration-test API

--- webapp/resource/integration-test-persistence/api/src/test/java/cmahy/webapp/resource/integration/test/persistence/api/annotation/TestPersistenceDataSource.java
+++
@@ -1,9 +0,0 @@
-package cmahy.webapp.resource.integration.test.persistence.api.annotation;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface TestPersistenceDataSource {
-}
